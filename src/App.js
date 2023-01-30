0ac58a255328f041e9f54250d1c3e07291746c81
samanfathnazarian <saman.nazarian17@gmail.com>
2023-01-29 18:16:42 -0600

    Optimized code by using async/await

--- src/App.js
+++ src/App.js
@@ -5,9 +5,43 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   // Making the fetch inside of App.js as doing it inside MoviesList would make that component non-reusable, it's good practice to have states in the parent component rather than child components
-  function fetchMovieHandler() {
+  async function fetchMovieHandler() {
+    // setIsLoading(true);
+
+    const response = await fetch('https://swapi.dev/api/films');
+    const data = await response.json(); //translate json response body to js object
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.releaseDate,
+      };
+    });
+
+    setMovies(transformedMovies);
+    // setIsLoading(false);
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
+
+/*
+    //CODE IN fetchMovieHandler() USING THEN, INSTEAD OF ASYNC AWAIT
     fetch('https://swapi.dev/api/films')
       .then((response) => {
         return response.json(); //translate json response body to js object
@@ -25,18 +59,4 @@ function App() {
 
         setMovies(transformedMovies);
       });
-  }
-
-  return (
-    <React.Fragment>
-      <section>
-        <button onClick={fetchMovieHandler}>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={movies} />
-      </section>
-    </React.Fragment>
-  );
-}
-
-export default App;
+      */
